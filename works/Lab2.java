efee8e4d7775781eb31cdc552aef76b218444109
Dvimin <dmitriev.michail@mail.ru>
2023-12-09 23:34:16 +0300

    2 laba

--- works/Lab2.java
+++ works/Lab2.java
@@ -1,4 +1,20 @@
 package works;
-
+import java.util.Scanner;
 public class Lab2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите слова (для завершения введите '!!'):");
+
+        while (scanner.hasNext()) {
+            String word = scanner.next();
+
+            if (word.equalsIgnoreCase("!!")) {
+                break; // Выход из цикла при вводе '!!'
+            }
+            System.out.println("Вы ввели: " + word);
+        }
+
+        scanner.close();
+    }
 }
